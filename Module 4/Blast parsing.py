7a801d3b71136718b6de68c8069f78d4854692b8
Timothy <30318121+TimothyBergstrom@users.noreply.github.com>
2017-11-17 14:10:00 +0100

    Done with script

--- Module 4/Blast parsing.py
+++ Module 4/Blast parsing.py
@@ -1,32 +1,50 @@
 import Bio
 import os
 from Bio import SeqIO
-from Bio.Blast import NCBIWWW, NCBIXML
+from Bio import SearchIO
+from Bio.Blast import NCBIWWW
+from Bio.Blast import NCBIXML
 import sys
 import re
 import time
 
-#Stupid way, but ignore warnings when not multiple of 3
-import warnings
-warnings.filterwarnings("ignore")
-
-os.chdir('files')
-file_list = sys.argv[1:]
-
 start_time = time.time()
+os.chdir('files')
+file_list = sys.argv[2:]
+regex_search = sys.argv[1]
 
-print('Start of run\n')
+print('\nStart of run')
 for file_name in file_list:
-    with open(file_name,'r') as f:
-        xml_read = NCBIXML.read(f)
-        for alignment in xml_read.alignments:
+    print(file_name)
+    found_hit = False   
+    file = open(file_name)
+    blast_records = NCBIXML.parse(file)
+    for blast_record in blast_records:
+        for alignment in blast_record.alignments:
             for hsp in alignment.hsps:
-                print(alignment.title)
-                print(alignment.length)
+                if bool(re.search(regex_search.upper(), str(alignment.title))):
+
+                    if not found_hit:
+                        print('Query name\tHit name\tScore\tE-value')
+
+                    found_hit = True                    
+                    index_word= re.search(regex_search.upper(),
+                                          alignment.title)
+                    print(blast_record.query + '\t' +
+                          alignment.title[index_word.start():index_word.start()+20] + '\t' +
+                          str(hsp.score) + '\t' +
+                          str(hsp.expect) + '\t')
+
+                    
+    file.close()
 
 
-
-        
+if not found_hit:
+    print('No hits found')
+    
 stop_time = time.time()
 
 print('\nIt took {} seconds'.format(stop_time-start_time))
+
+
+#https://www.biostars.org/p/193393/
