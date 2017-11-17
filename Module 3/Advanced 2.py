b22b15c416b8e7511f66ee4de2b965a2de582b25
Timothy <30318121+TimothyBergstrom@users.noreply.github.com>
2017-11-17 14:41:09 +0100

    Done with script!! Added debug

--- Module 3/Advanced 2.py
+++ Module 3/Advanced 2.py
@@ -28,7 +28,7 @@ def read_fasta(file_contents):
 
 def check_errors(arg_list):
     ##Error handling
-    if len(arg_list) != 2:
+    if len(arg_list) == 1:
         print('No bootstrap value added!')
         print("""Usage:\n
                bootstrap <filename> <number of boostraps>""")
@@ -46,24 +46,28 @@ def check_errors(arg_list):
         print("""Usage:\n
                bootstrap <filename> <number of boostraps>""")
         quit()
-    return 'No errors'
+    try:
+	debug = arg_list[2]
+        if debug == 'True': # Input is string
+	    return True
+    except:
+        return False
 
 
 if __name__ == "__main__":
-    arg_list = sys.argv[1:] #First in list is the name of the script
+    arg_list = sys.argv[1:] # First in list is the name of the script
     os.chdir('files')
-    check_errors(arg_list)
+    debug_check = check_errors(arg_list) # Check for errors in arguments
     with open(arg_list[0], 'r') as f:
         names, sequences = read_fasta(f)
-    tmp_file = tempfile.NamedTemporaryFile(mode='w+', delete=False) #Opens in read and write mode
-    tmp_file.write(str(len(names)) + ' ' + str(len(sequences[0])) + '\n')
+    tmp_file = tempfile.NamedTemporaryFile(mode='w+', delete=False) # Opens in read and write mode
+    """Detected a problem: The name MUST be length 10, not less!! Fill with spaces"""
+    tmp_file.write(str(len(names)) + '\t' + str(len(sequences[0])) + '\n')
     for i in range(len(names)):
-        tmp_file.write(names[i] + ' ' + sequences[i] + '\n')
+        tmp_file.write(names[i] + ' '*(10 - len(names[i])) + sequences[i] + '\n')
     tmp_file.seek(0) #Go to start of file
     path_to_tmp = os.path.split(tmp_file.name)
     os.chdir(path_to_tmp[0])
-    print(tmp_file.read())
-    time.sleep(5)
     try:
 	os.remove('outfile') #Clear from previous runs
     except:
@@ -73,19 +77,53 @@ if __name__ == "__main__":
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
 
+    if debug_check:
+	print(tmp_file.read())
+	time.sleep(3)
+
     tmp_file.close()
     stderr, stdout = process.communicate(input=(path_to_tmp[1] + '\n'
 						'I' + '\n'
 						'Y' + '\n'))
-    print(stderr)
+    if debug_check:
+	print(stderr)
+	time.sleep(3)
     
+
     try:
 	os.remove(path_to_tmp[1]) #Clear from previous runs
     except:
 	pass
+    os.rename('outfile', 'outfile_tmp')
+    process = subprocess.Popen(['phylip','neighbor'],
+                               stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    stderr, stdout = process.communicate(input=('outfile_tmp' + '\n'
+						'Y' + '\n'))
+    
 
+    if debug_check:
+	print(stderr)
+	time.sleep(3)
 
+    os.remove('outfile_tmp')
+    sys.stderr.write('\x1b[2J\x1b[H') # Clear terminal
+    with open('outfile','r') as f1, open('outtree','r') as f2:
+	print(f2.read())
+	
+	start_print = False
+	for line in f1:
+	    if start_print:
+		print(line)
+	    if 'Negative' in line:
+		start_print = True
+	    elif 'remember' in line:
+		start_print = False
 
+    os.remove('outfile')
+    os.remove('outtree')
+	
 
 
 
