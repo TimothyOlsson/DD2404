aa6bddc59f36f8fda3ce446a4f78a1d620c9eac4
Timothy <30318121+TimothyBergstrom@users.noreply.github.com>
2017-11-17 14:10:54 +0100

    A lot of fixing. Dist matrix might work now

--- Module 3/Advanced 2.py
+++ Module 3/Advanced 2.py
@@ -9,7 +9,7 @@ import subprocess
 import tempfile
 
 # Function, reading fasta files
-def read_fasta(self, file_contents):
+def read_fasta(file_contents):
     """Input: list from fasta file, Output: 2 lists with names and sequences"""
     names = []
     sequences = []
@@ -17,58 +17,76 @@ def read_fasta(self, file_contents):
     for line in file_contents:
         line = str(line) #Bug with empty comments
         if line.startswith('>'):
-            names.append(line.rstrip('\n'))
+            names.append(line[:9].rstrip('\n').strip('>').rstrip('\r'))
             if _seq != []:
-                sequences.append(''.join(_seq))
+                sequences.append(''.join(_seq).rstrip('\r'))
             _seq = []
         else:
-            _seq.append(line.rstrip('\n').upper())
+            _seq.append(line.rstrip('\n').rstrip('\r').upper())
     sequences.append(''.join(_seq).upper()) #upper case
     return names, sequences
 
-arg_list = sys.argv[1:] #First in list is the name of the script
-os.chdir('files')
+def check_errors(arg_list):
+    ##Error handling
+    if len(arg_list) != 2:
+        print('No bootstrap value added!')
+        print("""Usage:\n
+               bootstrap <filename> <number of boostraps>""")
+        quit()
+    if os.path.splitext(arg_list[0])[1] not in ['.fa','.FA','.fasta','.FASTA']:
+        print('Wrong file type used. Only use fasta files!')
+        quit()
+    elif not os.path.isfile(arg_list[0]):
+        print('File not found: %s' % arg_list[0])
+        quit()
+    try:
+        int(arg_list[1]) #Check if the value is a number
+    except:
+        print('Bootstrap value is not a number')
+        print("""Usage:\n
+               bootstrap <filename> <number of boostraps>""")
+        quit()
+    return 'No errors'
 
-##Error handling
-if len(arg_list) != 2:
-    print('No bootstrap value added!')
-    print("""Usage:\n
-           bootstrap <filename> <number of boostraps>""")
-    quit()
-if os.path.splitext(arg_list[0])[1] not in ['.fa','.FA','.fasta','.FASTA']:
-    print('Wrong file type used. Only use fasta files!')
-    quit()
-elif not os.path.isfile(arg_list[0]):
-    print('File not found: %s' % arg_list[0])
-    quit()
 
-try:
-    int(arg_list[1])
-except:
-    print('Bootstrap value is not a number')
-    print("""Usage:\n
-           bootstrap <filename> <number of boostraps>""")
-    quit()
+if __name__ == "__main__":
+    arg_list = sys.argv[1:] #First in list is the name of the script
+    os.chdir('files')
+    check_errors(arg_list)
+    with open(arg_list[0], 'r') as f:
+        names, sequences = read_fasta(f)
+    tmp_file = tempfile.NamedTemporaryFile(mode='w+', delete=False) #Opens in read and write mode
+    tmp_file.write(str(len(names)) + ' ' + str(len(sequences[0])) + '\n')
+    for i in range(len(names)):
+        tmp_file.write(names[i] + ' ' + sequences[i] + '\n')
+    tmp_file.seek(0) #Go to start of file
+    path_to_tmp = os.path.split(tmp_file.name)
+    os.chdir(path_to_tmp[0])
+    print(tmp_file.read())
+    time.sleep(5)
+    try:
+	os.remove('outfile') #Clear from previous runs
+    except:
+	pass
+    process = subprocess.Popen(['phylip','protdist'],
+                               stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
 
-tmp_file = tempfile.NamedTemporaryFile(mode='w+') #Opens in read and write mode
-print(arg_list)
-with open(arg_list[0], 'r') as f:
-    for i in f:
-        tmp_file.write(i)
+    tmp_file.close()
+    stderr, stdout = process.communicate(input=(path_to_tmp[1] + '\n'
+						'I' + '\n'
+						'Y' + '\n'))
+    print(stderr)
+    
+    try:
+	os.remove(path_to_tmp[1]) #Clear from previous runs
+    except:
+	pass
 
-tmp_file.seek(0) #Go to start of file
-path_to_tmp = os.path.split(tmp_file.name)
 
-os.chdir(path_to_tmp[0])
 
-process = subprocess.Popen(['phylip','neighbor'],
-                           stdin=subprocess.PIPE,
-                           stdout=subprocessPIPE,
-                           stderr=subprocess.PIPE)
 
-process.communicate(path_to_tmp[1])
-
-#process = subprocess.call(['phylip','neighbor', [arg for sublist in lists for item in sublist]
 
 
 
